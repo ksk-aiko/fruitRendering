c065150bbad25b9fcf82cc8b055837b6f0c34e67
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-07-26 14:48:03 +0900

    Refactoring: Define URLs as constants outside the function to improve performance.

--- fruitImgUrl.js
+++ fruitImgUrl.js
@@ -1,38 +1,36 @@
-/**
- * Get the image URL for a specific fruit.
- * @param {string} fruit - The name of the fruit.
- * @returns {string} - The image URL of the fruit.
- */
-function fruitImgUrl(fruit) {
-    const fruitTable = {
-        "banana": "https://www.kroger.com/product/images/xlarge/front/0000000004011",
-        "pineapple": "https://www.plantgrower.org/uploads/6/5/5/4/65545169/published/pineapple.jpg?1516498929",
-        "pear": "https://www.producemarketguide.com/sites/default/files/Commodities.tar/Commodities/pears_commodity-page.png"
-    }
-    const defaultUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Culinary_fruits_front_view.jpg";
+const FRUIT_URL_TABLE = {
+  banana: "https://www.kroger.com/product/images/xlarge/front/0000000004011",
+  pineapple:
+    "https://www.plantgrower.org/uploads/6/5/5/4/65545169/published/pineapple.jpg?1516498929",
+  pear: "https://www.producemarketguide.com/sites/default/files/Commodities.tar/Commodities/pears_commodity-page.png",
+};
 
-    // Get the URL from the fruitTable or return the default URL if not found
-    return fruitTable[fruit] || defaultUrl;
+const DEFAULT_FRUIT_URL =
+  "https://upload.wikimedia.org/wikipedia/commons/2/2f/Culinary_fruits_front_view.jpg";
+
+function fruitImgUrl(fruit) {
+  // Get the URL from the fruitTable or return the default URL if not found
+  return FRUIT_URL_TABLE[fruit] || DEFAULT_FRUIT_URL;
 }
 
 // 入力されたfruitを要素に挿入する関数
 function renderFruit(fruit, ele) {
-    // 要素の作成
-    let fruitP = document.createElement("p");
-    fruitP.innerHTML = "Our fruit is : " + fruit;
-    // img要素の作成
-    let fruitImg = document.createElement("img");
-    // imgタグへsrc属性の追加
-    fruitImg.src = fruitImgUrl(fruit);
-    // imgタグへclass属性の追加
-    fruitImg.classList.add("fruitImg");
-    // div要素に入れる
-    let fruitDiv = document.createElement("div");
-    fruitDiv.classList.add("fruitDiv");
-    fruitDiv.append(fruitP);
-    fruitDiv.append(fruitImg);
-    // コンテナの中に入れる
-    ele.append(fruitDiv);
+  // 要素の作成
+  let fruitP = document.createElement("p");
+  fruitP.innerHTML = "Our fruit is : " + fruit;
+  // img要素の作成
+  let fruitImg = document.createElement("img");
+  // imgタグへsrc属性の追加
+  fruitImg.src = fruitImgUrl(fruit);
+  // imgタグへclass属性の追加
+  fruitImg.classList.add("fruitImg");
+  // div要素に入れる
+  let fruitDiv = document.createElement("div");
+  fruitDiv.classList.add("fruitDiv");
+  fruitDiv.append(fruitP);
+  fruitDiv.append(fruitImg);
+  // コンテナの中に入れる
+  ele.append(fruitDiv);
 }
 
 // 実行プログラム
